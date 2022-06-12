07d38d9828850c533d379b1bb4a4f57470d4eea5
AlvinLee66 <ewqaz001@gmail.com>
2022-06-12 21:24:37 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const Record = require('./models/record')
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
@@ -13,7 +14,10 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-  res.render('index')
+  Record.find()
+  .lean()
+  .then(records => res.render('index', { records }))
+  .catch(err => console.log(err))
 })
 
 app.listen(PORT, () => {
