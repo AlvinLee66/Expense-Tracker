1855504f75a9ddd7336399c942861e6dca10b5f3
AlvinLee66 <ewqaz001@gmail.com>
2022-06-16 09:47:39 +0800

    feat: add create record function

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const Record = require('./models/record')
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -13,11 +14,24 @@ require('./config/mongoose')
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
   Record.find()
   .lean()
   .then(records => res.render('index', { records }))
   .catch(err => console.log(err))
+})
+
+app.get('/records/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/records', (req, res) => {
+  const { name, date, amount, category } = req.body
+  return Record.create({ name, date, amount, category })
+    .then(() => res.redirect('/')) 
+    .catch(err => console.log(err))
 })
 
 app.listen(PORT, () => {
