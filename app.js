2dc13a6a93903590369110e06695c756bc95b1b7
AlvinLee66 <ewqaz001@gmail.com>
2022-06-22 20:09:58 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const exphbs = require('express-handlebars')
 const session = require('express-session')
 const methodOverride = require('method-override')
 const bodyParser = require('body-parser')
+const usePassport = require('./config/passport')
 const routes = require('./routes')
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -22,6 +23,7 @@ app.use(session({
 }))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+usePassport(app)
 app.use(routes)
 
 app.listen(PORT, () => {
